d20afba866bfff6c5b3a8c032b3fcae2a1349803
tjb346 <tbrockhoeft@texastribune.org>
2018-11-30 15:49:05 -0600

    Don't throw if no DSN

--- sentry.py
+++ sentry.py
@@ -4,13 +4,7 @@ class ErrorHandler(object):
     def __init__(self, config):
         import sentry_sdk
 
-        dsn = config.SENTRY_DSN_URL
-        if not dsn:
-            raise RuntimeError(
-                "If you set USE_CUSTOM_ERROR_HANDLING to True, and you are using thumbor.error_handlers.sentry, " +
-                "then you must specify the Sentry DSN using the SENTRY_DSN_URL configuration."
-            )
-
+        dsn = config.SENTRY_DSN_URL or None
         env = config.SENTRY_ENVIRONMENT or "test"
 
         self.sentry = sentry_sdk.init(

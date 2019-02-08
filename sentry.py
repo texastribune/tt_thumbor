271551c1b0767d0d34f33f27f74a4d4ca3d59676
tjb346 <tbrockhoeft@texastribune.org>
2019-02-08 11:01:45 -0600

    Add version logging

--- sentry.py
+++ sentry.py
@@ -2,34 +2,40 @@ import sentry_sdk
 from sentry_sdk.integrations.logging import ignore_logger
 from sentry_sdk.utils import event_from_exception, exc_info_from_error
 
+from thumbor import __version__
+
 
 class ErrorHandler(object):
     def __init__(self, config):
         dsn = config.SENTRY_DSN_URL
-        env = config.SENTRY_ENVIRONMENT or "test"
 
         if not dsn:
             raise RuntimeError(
-                "If you set USE_CUSTOM_ERROR_HANDLING to True, and you are using thumbor.error_handlers.sentry, " +
+                "If you set USE_CUSTOM_ERROR_HANDLING to True, and you are using thumbor.error_handlers.sentry_python, " +
                 "then you must specify the Sentry DSN using the SENTRY_DSN_URL configuration."
             )
+
+        kwargs = {
+            'dsn': dsn,
+            'integrations': [],
+        }
+
+        env = config.get('SENTRY_ENVIRONMENT')
+        if env is not None:
+            kwargs['environment'] = env
 
         # Logging integration will create duplicates if below not ignored
         ignore_logger('thumbor')
         ignore_logger('tornado.access')
 
-        sentry_sdk.init(
-            dsn=dsn,
-            environment=env,
-            integrations=[]
-        )
+        sentry_sdk.init(**kwargs)
 
     def handle_error(self, context, handler, exception):
         req = handler.request
 
         exc_info = exc_info_from_error(exception)
 
-        with sentry_sdk.push_scope():
+        with sentry_sdk.push_scope() as scope:
             event, hint = event_from_exception(exc_info)
             event["request"] = {
                 "url": req.full_url(),
@@ -39,4 +45,5 @@ class ErrorHandler(object):
                 "headers": req.headers,
                 "body": req.body,
             }
+            scope.set_extra('thumbor-version',  __version__)
             sentry_sdk.capture_event(event, hint=hint)
